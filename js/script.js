0b11ba227ce561185ab6bdb9a4ffd425bf71c483
Tamzid Sakib <93206260+tamzidsakib@users.noreply.github.com>
2022-03-04 12:53:35 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// JS
+
+
+
+
+
+
+// jq
+
+$(document).ready(function(){
+
+// jquery ui
+
+$( "#draggable1" ).draggable();
+
+
+$( "#draggable2" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+
+
+$( "#sortable" ).sortable();
+
+$( "#accordion" ).accordion();
+
+
+var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+
+
+
+    $( "#datepicker" ).datepicker();
+
+    $( "#menu" ).menu();
+
+
+    $( "#speed" ).selectmenu();
+ 
+    $( "#files" ).selectmenu();
+ 
+    $( "#number" )
+      .selectmenu()
+      .selectmenu( "menuWidget" )
+        .addClass( "overflow" );
+ 
+    $( "#salutation" ).selectmenu();
+
+
+    $( "#tabs" ).tabs();
+
+    $( document ).tooltip();
+
+
+
+
+   
+
+
+
+
+
+     
+});
